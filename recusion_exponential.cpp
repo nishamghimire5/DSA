64558e7e018441dd12a9face884305db0dc342b8
Nisham Ghimire <nishamghimire5@gmail.com>
2021-09-23 15:43:54 +0545

    optimize recurssion for exponent

--- recusion_exponential.cpp
+++ recusion_exponential.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int power(int m,int n)
 {
-    if (m == 0)
+    if (n == 0)
         return 1;
     
-    if (n%2==0)
+    if (n%2==0){
         return power(m * m, n / 2);
-    else
-        return m * power(m * m, (n - 1) / 2);
+    }
+    else{
+    return m * power(m * m, (n - 1) / 2);
+    }
 }
 int main(){
     cout << "Enter in the form m^n separated with spaces : " << endl;
